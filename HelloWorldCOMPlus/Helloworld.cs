cc9b49cefd13e810e580af6874bd4c58b18c2ee4
nshaner <nathandshaner@hotmail.com>
2019-10-09 18:29:42 -0400

    Initial Upload of my COM+

---
+++ HelloWorldCOMPlus/Helloworld.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCOMPlus
+{
+    public class HelloWorld: ServicedComponent, IHelloWorld
+    {
+        public string Display()
+        {
+            try
+            {
+                return "I wrote a complus component on xbox Chandra!";
+            }
+            catch
+            {
+                return "Oh no! You hit an error!";
+            }
+            finally
+            {
+                Dispose();
+            }
+        }
+        public int Addition(int a, int b)
+        {
+            try
+            {
+                return a + b;
+            }
+            catch
+            {
+                return 0;
+            }
+            finally
+            {
+                Dispose();
+            }
+        }
+        public int Subtraction(int a, int b)
+        {
+            try
+            {
+                return a - b;
+            }
+            catch
+            {
+                return 0;
+            }
+            finally
+            {
+                Dispose();
+            }
+        }
+
+    }
+}
---
+++ HelloWorldCOMPlus/IHelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCOMPlus
+{
+    public interface IHelloWorld
+    {
+        string Display();
+
+        int Addition(int a, int b);
+
+        int Subtraction(int a, int b);
+
+    }
+}
